0261a58fbec49b03ce7f7f903936b173f341f46b
Siddharth Bhadra-Lobo <sbhadralobo@ucdavis.edu>
2014-10-20 13:21:56 -0700

    seqq -> scythe -> seqtk -> seqq -> bwa-mem -> samtools

--- slurm_scripts/Preprocessing_and_Alignment_Pipeline.sh
+++ slurm_scripts/Preprocessing_and_Alignment_Pipeline.sh
@@ -4,8 +4,8 @@
 ###### Cleaning up reads for alignment.
 ########
 
-#SBATCH -D /home/sbhadral/Projects/Rice_project/
-#SBATCH -J PAAP_test
+#SBATCH -D /home/sbhadral/Projects/Rice_project/rice_fastqs/
+#SBATCH -J PAAP_bc1
 #SBATCH -p serial
 #SBATCH -o /home/sbhadral/Projects/Rice_project/outs/%j.out
 #SBATCH -e /home/sbhadral/Projects/Rice_project/errors/%j.err
@@ -15,16 +15,19 @@
 #SBATCH --mail-user=sbhadralobo@ucdavis.edu
 set -e
 set -u
+#SBATCH --array=1
 
 ###### command line input. for file in $(ls *.gz); do sbatch /home/sbhadral/Projects/scripts/Preprocessing_and_Alignment_Pipeline.sh "$file" ; done
 
+module load bwa/0.7.5a
 
-#### First, unzip .gz files, then sort reads using NGSUtils.
+FILE=$( sed -n "$SLURM_ARRAY_TASK_ID"p /home/sbhadral/Projects/Rice_project/rice_fastqs/backcross_test.txt )
 
-cd /home/sbhadral/Projects/Rice_project/test_dir/
+## Before alignment, index reference. bwa index -p O_sativa Oryza_sativa.IRGSP-1.0.23.dna.genome.fa.gz
+
 
 # Initialize a list to run loop through.
-for file1 in $(ls *R1*.gz);
+for file1 in "$FILE";
 
 do
 
@@ -37,38 +40,55 @@ echo $file1
 echo $file2
 echo $file3
 
-## Sort each run, while directing tempfiles to a staging directory, then save as $file[1-2].sort
-/home/sbhadral/Projects/Rice_project/ngsutils/bin/fastqutils sort -T /home/sbhadral/Projects/Rice_project/pre_alignment <(gunzip -dkcf $file1)  > /home/sbhadral/Projects/Rice_project/pre_alignment/$file1.sort 
-
-/home/sbhadral/Projects/Rice_project/ngsutils/bin/fastqutils sort -T /home/sbhadral/Projects/Rice_project/pre_alignment <(gunzip -dkcf $file2) 	> /home/sbhadral/Projects/Rice_project/pre_alignment/$file2.sort
-
-
-echo $file1.sort
-echo $file2.sort
-
 cd /home/sbhadral/Projects/Rice_project/pre_alignment/
-
-#Find properly paired reads (when fragments are filtered separately).
-/home/sbhadral/Projects/Rice_project/ngsutils/bin/fastqutils properpairs $file1.sort $file2.sort $file1.sort.pair $file2.sort.pair
-
-echo $file1.sort.pair
-echo $file2.sort.pair
-
-# Merge the sorted runs into a single file.
-/home/sbhadral/Projects/Rice_project/ngsutils/bin/fastqutils merge $file1.sort.pair $file2.sort.pair > $file3.sort.pair.merge 
-
-# Check with echos
-echo $file3.sort.pair.merge
-
-###### Run reads through seqqs, which records metrics on read quality, length, base composition.
 
 mkdir $file3
 
-## somethings going on here, but not enough battery power left to check it just yet. Saying /$file3/$file3.sort.pair.merge.seqq doesn't exist but the output is there...
-cat $file3.sort.pair.merge | /home/sbhadral/Projects/Rice_project/seqqs/seqqs - -e -i -p raw.$file3-$(date +%F) >  /home/sbhadral/Projects/Rice_project/pre_alignment/$file3.sort.pair.merge.seqq
+cd /home/sbhadral/Projects/Rice_project/rice_fastqs/
 
-	echo $file3.sort.pair.merge.seqq
+###### First, unzip .gz files, then sort reads using NGSUtils. 
+###### Sort each run, while directing temp files to a staging directory, then save as $file[1-2].sort
+
+	/home/sbhadral/Projects/Rice_project/ngsutils/bin/fastqutils sort -T /home/sbhadral/Projects/Rice_project/pre_alignment/ <(gunzip -dkcf $file1)  > $file1.sort 
+
+	/home/sbhadral/Projects/Rice_project/ngsutils/bin/fastqutils sort -T /home/sbhadral/Projects/Rice_project/pre_alignment/ <(gunzip -dkcf $file2)  > $file2.sort
+
+	#### Trying to streamline it.
+	#/home/sbhadral/Projects/Rice_project/ngsutils/bin/fastqutils sort -T /home/sbhadral/Projects/Rice_project/pre_alignment <(gunzip -dkcf $file1) <(gunzip -dkcf $file2) |
+
+		echo $file1.sort
+		echo $file2.sort
+
+
+#Find properly paired reads (when fragments are filtered separately).
+
+	/home/sbhadral/Projects/Rice_project/ngsutils/bin/fastqutils properpairs  $file1.sort  $file2.sort $file1.sort.pair $file2.sort.pair
+
+		echo $file1.sort.pair
+		echo $file2.sort.pair
+
+	#### Trying to streamline it.
+	#/home/sbhadral/Projects/Rice_project/ngsutils/bin/fastqutils properpairs - > $file3.sort.pair
+
+	#echo $file3.sort.pair
+
+# Merge the sorted runs into a single file.
+
+	/home/sbhadral/Projects/Rice_project/ngsutils/bin/fastqutils merge $file1.sort.pair $file2.sort.pair > $file3.sort.pair.merge 
+
+		echo $file3.sort.pair.merge
+
+###### Run reads through seqqs, which records metrics on read quality, length, base composition.
+
+	cat $file3.sort.pair.merge | /home/sbhadral/Projects/Rice_project/seqqs/seqqs - -e -i -p raw.$file3-$(date +%F) >  $file3.sort.pair.merge.seqq
+
+		echo $file3.sort.pair.merge.seqq
 	
+	#### Trying to streamline it.
+
+	#cat $file3.sort.pair | /home/sbhadral/Projects/Rice_project/ngsutils/bin/fastqutils merge - > $file3.sort.pair.merge
+
+	#echo $file3.sort.pair.merge
 
 ###### Trim adapter sequences off of reads using scythe.
 
@@ -76,28 +96,48 @@ cat $file3.sort.pair.merge | /home/sbhadral/Projects/Rice_project/seqqs/seqqs - 
 
 	echo $file3.sort.pair.merge.seqq.scythe
 
+
+	#### Trying to streamline it.
+	
+	#cat $file3.sort.pair.merge | /home/sbhadral/Projects/Rice_project/scythe/scythe --quiet -a /home/sbhadral/Projects/Rice_project/scythe/illumina_adapters.fa - |
+
+
 ###### Quality-based trimming with seqtk's trimfq.
 
-	cat $file3.sort.pair.merge.seqq.scythe | /home/sbhadral/Projects/Rice_project/seqtk/seqtk trimfq -q 0.01 - > /home/sbhadral/Projects/Rice_project/pre_alignment/$file3.sort.pair.merge.seqq.scythe.trimmed
+		cat $file3.sort.pair.merge.seqq.scythe | /home/sbhadral/Projects/Rice_project/seqtk/seqtk trimfq -q 0.01 - > $file3.sort.pair.merge.seqq.scythe.trimmed
 	
-mv raw.$file3-* /home/sbhadral/Projects/Rice_project/pre_alignment/$file3/ 
+			mv raw.$file3-* /home/sbhadral/Projects/Rice_project/$file3/ 
 			
-			echo $file3.sort.pair.merge.seqq.scythe.trimmed
-			
-			cat $file3.sort.pair.merge.seqq.scythe.trimmed | /home/sbhadral/Projects/Rice_project/seqqs/seqqs - -e -i -p trimmed.$file3-$(date +%F) 
-			
-				> /home/sbhadral/Projects/Rice_project/pre_alignment/$file3.sort.pair.merge.seqq.scythe.trimmed.out
+				echo $file3.sort.pair.merge.seqq.scythe.trimmed
 
-		echo $file3.sort.pair.merge.seqq.scythe.trimmed.out		
+	#### Trying to streamline it.
+	# /home/sbhadral/Projects/Rice_project/seqtk/seqtk trimfq -q 0.01 - |
 
-mv trimmed.$file3-* /home/sbhadral/Projects/Rice_project/pre_alignment/$file3/
+###### Another around of seqqs, which records post pre-processing read quality metrics.			
+
+				cat $file3.sort.pair.merge.seqq.scythe.trimmed | /home/sbhadral/Projects/Rice_project/seqqs/seqqs - -e -i -p trimmed.$file3-$(date +%F) 
+			
+					> $file3.sort.pair.merge.seqq.scythe.trimmed.fq
+
+							echo $file3.sort.pair.merge.seqq.scythe.trimmed.fq		
+
+#### Trying to streamline it.
+#/home/sbhadral/Projects/Rice_project/seqqs/seqqs - -e -i -p trimmed.$file3-$(date +%F) - |
+
+# mv trimmed.$file3-* /home/sbhadral/Projects/Rice_project/pre_alignment/$file3/
+
+ 
+
+###### Align with BWA-MEM.
+
+	bwa mem -M -t 1 -v 1 -p /home/sbhadral/Projects/Rice_project/ref_gens/O_sativa $file3.sort.pair.merge.seqq.scythe.trimmed.fq | 
+	
+		 samtools view -Sbhu - > /home/sbhadral/Projects/Rice_project/alignments/check.$file3.bam
+
 
 mv *$file3*.sort* -f /home/sbhadral/Projects/Rice_project/pre_alignment/$file3/ ;
 
 done
 
 
-
-
-###### Another around of seqqs, which records post pre-processing read quality metrics.
 
